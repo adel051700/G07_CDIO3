a2349de6f9060ae5ae842940cda5d83fee98220d
Mustafa Baker <mustafabaker2970@gmail.com>
2023-11-03 08:28:49 +0100

    Created Dice.class

--- Dice.java
+++ Dice.java
@@ -1,3 +1,18 @@
+import java.util.Random;
+
 class Dice {
     
+    private int dieSides;
+
+    public Dice(int dieSides) {
+        this.dieSides = dieSides;
+
+    }
+
+    public int roll() {
+        Random random = new Random();
+        int randomInt = random.nextInt(this.dieSides);
+
+        return randomInt + 1;
+    }
 }
