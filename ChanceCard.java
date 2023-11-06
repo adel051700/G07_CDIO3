6000afaa27a406fc0c2136521f1a863de233e30d
Adel Ahmad <adel051700@gmail.com>
2023-11-06 16:09:46 +0100

    Added to ChanceCard class. Added that the class reads from a csv file, and created an array that contains the different valuess

--- ChanceCard.java
+++ ChanceCard.java
@@ -2,10 +2,19 @@ import java.io.File;
 
 public class ChanceCard 
 {    
+    private String name;
+    private String text;
     private int tilesToMove;
+    private int MoneyToChange;
+
     private int deltaBalance;
-    ChanceCard()
+    ChanceCard(String name, String text, int tilesToMove, int MoneyToChange)
     {
+        this.name=name;
+        this.text=text;
+        this.tilesToMove=tilesToMove;
+        this.MoneyToChange=MoneyToChange;
+
     }
 
     public void changeDeltaBalance(int changeBalance)
@@ -15,5 +24,28 @@ public class ChanceCard
 
     public static ChanceCard[] getChanceCardsFromFile(String language)
     {
+        int numberOfChancecard=20;
+        ChanceCard[] Chancearr= new ChanceCard[numberOfChancecard];
+        try{
+            var fileToRead= new File(language +".csv");
+            var Scanner = new java.util.Scanner(fileToRead);
+            int i=0;
+            while (Scanner.hasNextLine()){
+                String chanceCard=Scanner.nextLine();
+                String[] ChanceCards=chanceCard.split(";");
+                String name=ChanceCards[0];
+                String text=ChanceCards[1];
+                int tilesToMove=Integer.parseInt(ChanceCards[2]);
+                int MoneyToChange=Integer.parseInt(ChanceCards[3]);
+                     
+                Chancearr[i]=new ChanceCard(name,text, tilesToMove, MoneyToChange);
+                i++;
+            }
+            Scanner.close();
+        }
+        catch (Exception e){
+            System.out.println(e + " Error");
+        }
+        return Chancearr;
     }
 }
