dce133b353953eca2cd74d9aeb8072a9916ffdda
Elias Larsen <elias@takefive.dk>
2023-11-06 15:06:31 +0100

    Added Relevant methods to chancecards

--- ChanceCard.java
+++ ChanceCard.java
@@ -1,3 +1,19 @@
-class ChanceCard {
-    
+import java.io.File;
+
+public class ChanceCard 
+{    
+    private int tilesToMove;
+    private int deltaBalance;
+    ChanceCard()
+    {
+    }
+
+    public void changeDeltaBalance(int changeBalance)
+    {
+        this.deltaBalance += changeBalance;
+    }
+
+    public static ChanceCard[] getChanceCardsFromFile(String language)
+    {
+    }
 }
