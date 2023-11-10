0e7c022a9514a72ff2aa7289ce86f6d2b8b98cce
Frederik Bastrup <frederik.bastrup@gmail.com>
2023-11-10 10:08:51 +0100

    added bank class file

--- Bank.java
+++ Bank.java
@@ -1,3 +1,19 @@
-class Bank {
-    
+public class Bank {
+private int balance;
+
+    public void changeBalance(int deltaBalance) {
+        this.balance += deltaBalance;
+        if (this.balance < 0) {
+            this.balance = 0;
+        }
+    }
+
+    public int getBalance() {
+        return this.balance;
+    }
+
+    public Bank() {
+        this.balance = balance;
+    }
+
 }
