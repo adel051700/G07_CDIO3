663a8fd46544cac74c710072e4805f307d6017e7
Frederik Bastrup <frederik.bastrup@gmail.com>
2023-11-14 16:27:31 +0100

    changed Monopoly.java so that it uses the Player[] players array

--- Monopoly.java
+++ Monopoly.java
@@ -17,17 +17,16 @@ class Monopoly {
 
         System.out.println("Welcome to Monopoly Jr.! How many players are you? Press 2, 3 or 4");
 
-        int numberOfPlayers = s.nextInt();
-        // We need to integrade this above method so it uses the getAmountOfPlayers
-        // method. Also we need to set boundaries and exceptions.
+        int n = s.nextInt();
+        Player[] players = new Player[n];
         Player player1 = new Player(1, "cat", 20);
         Player player2 = new Player(2, "dog", 20);
-        if (numberOfPlayers == 3) {
+        if (getAmountofplayers() == 3) {
             Player player3 = new Player(3, "car", 18);
             player1.setBankBalance(18);
             player2.setBankBalance(18);
 
-        } else if (numberOfPlayers == 4) {
+        } else if (getAmountofplayers() == 4) {
             Player player3 = new Player(3, "car",16);
             Player player4 = new Player(4, "boat", 16);
             player1.setBankBalance(16);
