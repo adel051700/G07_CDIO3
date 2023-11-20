cc8e16c0b9390c3fc30944e902ffaf97dc17c8cb
Frederik Bastrup <frederik.bastrup@gmail.com>
2023-11-20 16:38:40 +0100

    added draw method to chanceFiled class

--- field.java
+++ field.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class field {
     public String name;
 
@@ -8,7 +10,7 @@ public class field {
 
 class specialField extends field {
     private int changeBalance;
-    
+
     public specialField(String name, int changeBalance) {
         super(name);
         this.changeBalance = changeBalance;
@@ -17,36 +19,46 @@ class specialField extends field {
 
 }
 
-class chanceField extends field{
+class chanceField extends field {
 
+    public chanceField(String name) {
+        super(name);
+    }
+
+    public static int drawChanceCard() {
+        Random random = new Random();
+        int cardNumber = random.nextInt((19 - 0) + 1) + 0;
+        return cardNumber;
+    }
 }
 
-class prisonField extends field{
+class prisonField extends field {
     private boolean isInPrison;
 
-    public prisonField (String name){
+    public prisonField(String name) {
         super(name);
     }
 }
 
-class buyableField extends field{
+class buyableField extends field {
     private int value;
     private String color;
     private int rent;
     private String[] owner;
 
-    public buyableField (String name, int value, String color, int rent, String[] owner){
+    public buyableField(String name, int value, String color, int rent, String[] owner) {
         super(name);
         this.value = value;
         this.color = color;
         this.rent = rent;
         this.owner = owner;
     }
-    public int getRent(){
+
+    public int getRent() {
         return this.rent;
     }
 
-    public void setRent(int multiplier){
-        this.rent=this.rent*multiplier;
+    public void setRent(int multiplier) {
+        this.rent = this.rent * multiplier;
     }
 }
