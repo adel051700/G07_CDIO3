05080b5fcb3308da873602cced6257a8b5052a67
Frederik Bastrup <frederik.bastrup@gmail.com>
2023-11-20 16:28:16 +0100

    added field hierarchy to the field.java file

--- field.java
+++ field.java
@@ -5,3 +5,48 @@ public class field {
         this.name = name;
     }
 }
+
+class specialField extends field {
+    private int changeBalance;
+    
+    public specialField(String name, int changeBalance) {
+        super(name);
+        this.changeBalance = changeBalance;
+
+    }
+
+}
+
+class chanceField extends field{
+
+}
+
+class prisonField extends field{
+    private boolean isInPrison;
+
+    public prisonField (String name){
+        super(name);
+    }
+}
+
+class buyableField extends field{
+    private int value;
+    private String color;
+    private int rent;
+    private String[] owner;
+
+    public buyableField (String name, int value, String color, int rent, String[] owner){
+        super(name);
+        this.value = value;
+        this.color = color;
+        this.rent = rent;
+        this.owner = owner;
+    }
+    public int getRent(){
+        return this.rent;
+    }
+
+    public void setRent(int multiplier){
+        this.rent=this.rent*multiplier;
+    }
+}
