30ca7cb02815297f9fda3ace3a0fca338e0d9dcd
Elias Larsen <elias@takefive.dk>
2023-11-23 15:03:24 +0100

    Added leaderboard functionality at the end of game

--- Monopoly.java
+++ Monopoly.java
@@ -6,7 +6,10 @@ class Monopoly {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int whoseTurn = 0;
+        while (true)
+        {
+
+        
         Dice dice1 = new Dice(6);
         Dice dice2 = new Dice(6);
 
@@ -46,55 +49,79 @@ class Monopoly {
             {
                 if (players[k].getBankBalance() >= 0)
                 {
-                    //Sets i value to 6 as of to break the outer for loop, and calculate the winner 
+                    //Sets i value to 6 to break the outer for loop, and calculate the winner(s) 
                     i = 6;
                 }
             }
         }
-        String[] leaderboard = new String[n];
-        int[] playerValue = new int[n];
-        int[] checkedPlayers = new int[n];
-        int maxValue = 0;
-        int playerNumber = 0;
+        Player[] leaderboard = new Player[n];
+        int[] sharedPlaces = new int[n];
         for (int i = 0; i < players.length; i++)
         {
-            for (int j = 0; j < players.length; j++)
+            int higherAmount = 0;
+            for (int k = 0; k < players.length; k++)
             {
-                if (players[j].getBankBalance() > maxValue)
+                if (players[i].getBankBalance() < players[k].getBankBalance() && i!=k)
                 {
-                    boolean doIt = true;
-                    for (int k = 0; k < checkedPlayers.length; k++)
-                    {
-                        if (checkedPlayers[k] == players[j].getNumber())
-                        {
-                            doIt = false;
-                        }
-                    }
-                    if (doIt)
-                    {
-                        maxValue = players[j].getBankBalance();
-                        playerNumber = players[j].getNumber();
-                        checkedPlayers[i] = playerNumber;
-                    }
+                    higherAmount++;
                     
                 }
+
+
+                if (k == players.length-1 && leaderboard[higherAmount] == null)
+                {
+                    leaderboard[higherAmount] = players[i];
+                }
+                else if (leaderboard[higherAmount] != null)
+                {
+                    for (int j = higherAmount; j < leaderboard.length; j++)
+                    {
+                        if (leaderboard[j] == null)
+                        {
+                            leaderboard[j] = players[i];
+                            sharedPlaces[j] = players[i].getBankBalance();
+                        }
+                    }
+                }
             }
-            leaderboard[i] = "Place Number " + (i+1) + " goes to: " + "Player number " + (i+1) + ", with a balance of " + maxValue;
-        }
-        for (int x = 0; x < leaderboard.length; x++)
-        {
-            System.out.println(leaderboard[x]);
         }
 
+        for (int i = 0; i < leaderboard.length; i++)
+        {
+            boolean share = false;
+            for (int k = 0; k < sharedPlaces.length; k++)
+            {
+                if (leaderboard[i].getBankBalance() == sharedPlaces[k])
+                {
+                    share = true;
+                }
+            }
+            if (!share)
+            {
+                System.out.println("Place "+ (i+1) + " goes to player: " + leaderboard[i].getNumber() + " with a score of: "  + leaderboard[i].getBankBalance());
+            }
+            else
+            {
+                String sharedStatement = "Place number " + (i+1) + " is shared by players: " + (i+1); 
+                for (int x = (i+1); x < leaderboard.length; x++)
+                {
+                    sharedStatement += ", " + (x+1);
+                    i++;
+                }
+                System.out.println(sharedStatement);
+            }
+                
+            
+        }
         
         
         }
         catch(IllegalArgumentException e)
         {
             System.out.println(e.getMessage());
+            s.next();
+            continue;
         }
-        {
-            
         }
     }
 }
